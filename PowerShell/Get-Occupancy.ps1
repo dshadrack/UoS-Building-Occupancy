a1abbee8a4c9a86ad88f44a3a941acd173c759c9
dshadrack <danshadrack@gmail.com>
2023-01-07 23:28:11 +0000

    Create Get-Occupancy.ps1

---
+++ PowerShell/Get-Occupancy.ps1
@@ -0,0 +1,16 @@
+﻿$CSVPATH = "F:\TEMP\Occupancy.csv"
+
+If (Test-Path -Path $CSVPATH){
+
+}Else{
+    Set-Content $CSVPATH -Value "DateTime,Occupancy"
+}
+
+$HTMLResponse = Invoke-WebRequest https://lib-wordpress.sheffield.ac.uk/occupancy/ic.htm?style=infoscreen 
+$SiteContent = $HTMLResponse.AllElements | Where {$_.TagName -eq "SPAN"}
+$Occupancy = $SiteContent.innerText
+$date = Get-Date -Format "MM/dd/yyyy HH:mm"
+
+$CSVfile = Import-Csv $CSVPATH
+$newline = $date + "," + $Occupancy
+$newline | Add-Content -Path $CSVPATH
