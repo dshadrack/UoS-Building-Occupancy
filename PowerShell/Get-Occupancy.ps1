491a5e6acda73a84e2057cea9d0c88c86df3d8b6
dshadrack <danshadrack@gmail.com>
2023-01-07 23:38:17 +0000

    Added comments to PS Script

--- PowerShell/Get-Occupancy.ps1
+++ PowerShell/Get-Occupancy.ps1
@@ -1,16 +1,26 @@
-﻿$CSVPATH = "F:\TEMP\Occupancy.csv"
+﻿#Define location for data to be saved
+$CSVPATH = "F:\TEMP\Occupancy.csv"
 
+#Check if data file exists 
 If (Test-Path -Path $CSVPATH){
 
+#If data file doesn't exist, create blank CSV file with column headers
 }Else{
     Set-Content $CSVPATH -Value "DateTime,Occupancy"
 }
 
+#Read the HTML from the specified site and load this into memory
 $HTMLResponse = Invoke-WebRequest https://lib-wordpress.sheffield.ac.uk/occupancy/ic.htm?style=infoscreen 
+#Isolate occupancy number from the returned HTML
 $SiteContent = $HTMLResponse.AllElements | Where {$_.TagName -eq "SPAN"}
 $Occupancy = $SiteContent.innerText
+
+#Get current date and time in a readable format
 $date = Get-Date -Format "MM/dd/yyyy HH:mm"
 
+#Load in CSV file to memory
 $CSVfile = Import-Csv $CSVPATH
+
+#Add newly returned data in a new line
 $newline = $date + "," + $Occupancy
 $newline | Add-Content -Path $CSVPATH
