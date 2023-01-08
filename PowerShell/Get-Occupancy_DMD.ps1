f98c1d6fdd5d08199d5c725b3d971be5cceb0a0b
dshadrack <danshadrack@gmail.com>
2023-01-08 13:46:57 +0000

    Create Get-Occupancy_DMD.ps1

---
+++ PowerShell/Get-Occupancy_DMD.ps1
@@ -0,0 +1,30 @@
+﻿#Define location for data to be saved
+$CSVPATH = "F:\TEMP\OccupancyDMD.csv"
+
+#Check if data file exists 
+If (Test-Path -Path $CSVPATH){
+
+#If data file doesn't exist, create blank CSV file with column headers
+}Else{
+    Set-Content $CSVPATH -Value "DateTime,Occupancy"
+}
+
+#Try/Catch to stop writing if disconnected fromt the internet
+try{
+
+    #Read the HTML from the specified site and load this into memory
+    $HTMLResponse = Invoke-WebRequest https://lib-wordpress.sheffield.ac.uk/occupancy/dmd.htm?style=infoscreen 
+    #Isolate occupancy number from the returned HTML
+    $SiteContent = $HTMLResponse.AllElements | Where {$_.TagName -eq "SPAN"}
+    $Occupancy = $SiteContent.innerText
+
+
+    #Get current date and time in a readable format
+    $date = Get-Date -Format "MM/dd/yyyy HH:mm"
+
+    #Add newly returned data in a new line
+    $newline = $date + "," + $Occupancy
+    $newline | Add-Content -Path $CSVPATH
+}
+
+Catch{}
